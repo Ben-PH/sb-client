e8ae8b660521ab2e6754e9c9ce648755f939f36a
Ben-PH <benphawke@gmail.com>
2020-08-05 01:54:51 +0200

    Impl UI to use login/out API

--- src/lib.rs
+++ src/lib.rs
@@ -1,94 +1,120 @@
+
 use seed::{prelude::*, *};
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+fn init(_: Url, orders: &mut impl Orders<Message>) -> Model {
+    log!("I N I T I A L I Z E");
+    // orders.subscribe(Message::UrlChanged);
+    // orders.send_msg(Message::GoToUrl(Url::new().set_path(&["login"])));
+    Model::default()
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
 struct Login {
     email: String,
     password: String,
 }
 
 // ------ ------
-//     Init
-// ------ ------
-
-fn init(_: Url, orders: &mut impl Orders<Message>) -> Model {
-    log!("I N I T I A L I Z E");
-    orders.subscribe(Message::UrlChanged);
-    orders.send_msg(Message::GoToUrl(Url::new().set_path(&["login"])));
-    Model::default()
-}
-
-// ------ ------
 //     Model
 // ------ ------
 
-#[derive(Default)]
-struct Model;
+#[derive(Debug, Default)]
+struct Model {
+    login: Login,
+    pub response_data: Option<String>,
+}
+#[derive(Debug)]
+enum Message{
+    EmailChanged(String),
+    PasswordChanged(String),
+    LoginButton,
+    LogoutButton,
+    CreateButton,
+    Fetched(fetch::Result<String>),
+}
+
 
 // ------ ------
 //    Update
 // ------ ------
 
-enum Message {
-    UrlChanged(subs::UrlChanged),
-    GoToUrl(Url),
-    LoginButton(String, String),
-    CreateButton(String, String),
-    LogoutButton(String),
-    ServerResponded(fetch::Result<String>),
-}
-
-fn update(msg: Message, _model: &mut Model, orders: &mut impl Orders<Message>) {
+fn update(msg: Message, model: &mut Model, orders: &mut impl Orders<Message>) {
     match msg {
-        Message::UrlChanged(change) => log!("changed to", change),
-        Message::GoToUrl(url) => {
-            log!("going to", url);
-            orders.notify(subs::UrlRequested::new(url));
+        Message::EmailChanged(email) => model.login.email = email,
+        Message::PasswordChanged(password) => model.login.password = password,
+        Message::LoginButton => {
+            let request = Request::new("/api/auth/login")
+                .method(Method::Post)
+                .json(&model.login)
+                .expect("Serialization failed");
+
+            orders.perform_cmd(
+                async {
+                    let response = fetch(request)
+                        .await
+                        .unwrap()
+                        .text()
+                        .await;
+
+                    Message::Fetched(response)
+                }
+            );
+        },
+        Message::Fetched(res) => match res {
+            Ok(response_data) => model.response_data = Some(response_data),
+            Err(res) => log!(res),
         }
-        Message::CreateButton(name, password) => {
-            log!("clickity clackity");
-            orders
-                .perform_cmd(async { Message::ServerResponded(send_create(name, password).await) });
+        Message::LogoutButton => {
+            let request = Request::new("/api/auth")
+                .method(Method::Delete);
+
+            orders.perform_cmd(
+                async {
+                    let response = fetch(request)
+                        .await
+                        .unwrap()
+                        .text()
+                        .await;
+
+                    Message::Fetched(response)
+                }
+            );
         }
-        Message::LoginButton(name, password) => {
-            log!("clickity clackity");
-            orders
-                .perform_cmd(async { Message::ServerResponded(send_login(name, password).await) });
-        }
-        Message::LogoutButton(_name) => {
-            log!("clickity clackity");
-            orders.perform_cmd(async {
-                Message::ServerResponded(send_logout("f@bar.com".to_string()).await)
-            });
-        }
-        Message::ServerResponded(res) => log!(res, cookies()),
-        // Message::ExampleE(msg) => {
-        //     example_e::update(msg, &mut model.example_e, &mut orders.proxy(Message::ExampleE));
-        // }
+        // Message::CreateButton => {
+        _ => log!("TODO: impl handling for ", msg)
     }
 }
 
-async fn send_logout(name: String) -> fetch::Result<String> {
-    Request::new(format!("/api/auth/{}", &name))
-        .method(fetch::Method::Delete)
-        .fetch()
-        .await?
-        .text()
-        .await
+async fn send_logout(orders: &mut impl Orders<Message>) {
+    let request = Request::new("/api/auth")
+        .method(Method::Delete);
+
+    orders.perform_cmd(
+        async {
+            let response = fetch(request)
+                .await
+                .unwrap()
+                .text()
+                .await;
+
+            Message::Fetched(response)
+        });
 }
 
-async fn send_login(email: String, password: String) -> fetch::Result<String> {
-    Request::new("/api/auth/login".to_string())
+async fn send_login(ser: &impl Serialize) -> fetch::Result<String> {
+    Request::new("/api/auth/login")
         .method(fetch::Method::Post)
-        .json(&Login { email, password })?
+        .json(ser)?
         .fetch()
         .await?
+    .check_status()?
         .text()
         .await
 }
 
 async fn send_create(email: String, password: String) -> fetch::Result<String> {
-    Request::new("/api/auth/create".to_string())
+    Request::new("/api/auth/create")
         .method(fetch::Method::Post)
         .json(&Login { email, password })?
         .fetch()
@@ -100,43 +126,17 @@ async fn send_create(email: String, password: String) -> fetch::Result<String> {
 //     View
 // ------ ------
 
-fn view(_model: &Model) -> impl IntoNodes<Message> {
-    let name = "f@bar.com";
-    vec![
-        button![
-            "click me to post to /api/auth/login".to_string(),
-            ev(Ev::Click, move |event| {
-                event.prevent_default();
-                Message::LoginButton(name.to_string(), "hunter2".to_string())
-            })
+fn view(model: &Model) -> Vec<Node<Message>> {
+    nodes![
+        input![
+            input_ev(Ev::Input, Message::EmailChanged),
         ],
-        button![
-            format!("click me to DELETE to /api/auth/{}", name),
-            ev(Ev::Click, move |event| {
-                event.prevent_default();
-                Message::LogoutButton(name.to_string())
-            })
+        input![
+            input_ev(Ev::Input, Message::PasswordChanged),
         ],
-        button![
-            "navigate to /login",
-            ev(Ev::Click, |event| {
-                event.prevent_default();
-                Url::new().set_path(&["login"]).go_and_load()
-            })
-        ],
-        button![
-            "Go to '/foo' and trigger `UrlChanged` (simulate `<a>` link click)",
-            ev(Ev::Click, |_| Message::GoToUrl(
-                Url::new().set_path(&["foo"])
-            ))
-        ],
-        button![
-            "create mr fooey",
-            ev(Ev::Click, move |event| {
-                event.prevent_default();
-                Message::CreateButton("f@bar.com".to_string(), "hunter2".to_string())
-            })
-        ],
+        button![ev(Ev::Click, |_| Message::LoginButton), "login"],
+        button![ev(Ev::Click, |_| Message::LogoutButton), "logout"],
+        button![ev(Ev::Click, |_| Message::CreateButton), "create"],
     ]
 }
 
